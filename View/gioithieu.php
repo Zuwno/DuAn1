c9262ce9d4046926d0d9a2d3c78ef67f1e8d07ec
[ngothehung] <[ngothehung2110@gmail.com]>
2022-12-12 22:50:26 +0700

    reponsive gioithieu

--- View/gioithieu.php
+++ View/gioithieu.php
@@ -5,18 +5,16 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=\, initial-scale=1.0">
-  <title>Taehui - Giới thiệu</title>
+  <title>Thực phẩm chức năng Taehui</title>
   <script src="https://cdn.tailwindcss.com"></script>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="shortcut icon" href="//theme.hstatic.net/200000557063/1000907375/14/favicon.png?v=1162" type="image/png">
 
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link
-    href="https://fonts.googleapis.com/css2?family=Heebo:wght@100;200;300;400;500;600;700;800;900&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Source+Code+Pro:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&family=Tinos:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@100;200;300;400;500;600;700;800;900&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Source+Code+Pro:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&family=Tinos:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
-    integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.8/slick-theme.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.8/slick.css">
@@ -24,17 +22,25 @@
   <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
   <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.5.7/slick.min.js"></script>
 
-
   <style>
     * {
       font-family: 'Tinos', serif;
-      
     }
-    
-    p{
-      font-size:18px;
+
+
+    .slick-carousel {
+      margin: 0 auto;
+      max-width: 1260px;
     }
-    
+
+    /* The width of each slide */
+    .slick-slide {}
+
+    /* Color of the arrows */
+    .slick-next::before,
+    .slick-prev::before {
+      color: black;
+    }
   </style>
 </head>
 
@@ -44,8 +50,8 @@
       <section class="bg-[#e76ea5] ">
         <div class="max-w-7xl m-auto flex justify-between text-white items-center ">
 
-          <span class="py-2 px-20 md:px-0">CÔNG TY CỔ PHẦN TAEHUI</span>
-          <div class="flex items-center space-x-3">
+          <span class="py-2 px-20 ">CÔNG TY CỔ PHẦN TAEHUI</span>
+          <div class="flex items-center space-x-3 hidden md:block">
 
             <a href=""><i class="fa fa-envelope px-2 " aria-hidden="true"></i>taehuivietnam@gmail.com </a>
 
@@ -54,27 +60,30 @@
             <a href=""><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
             <a href=""><i class="fa fa-google-plus-square" aria-hidden="true"></i></a>
             <a href=""><i class="fa fa-youtube-square" aria-hidden="true"></i></a>
-            <a href="cart.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i></i></a>
-            
+            <a href="Model/cart.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i></i></a>
           </div>
 
         </div>
       </section>
-      <section class=" max-w-8xl m-auto  py-2 relative z-[999] shadow-lg  ">
-        <div class="flex  justify-between items-center  px-28 bg-white">
-        <a href="../index.php"><img class="w-[180px]" src="../imgs/header_logo.png" alt=""></a>
-          <div>
-            <ul class="flex gap-4 font-bold ">
+      <section class=" max-w-8xl m-auto  py-2 relative z-[999] shadow-lg   ">
+        <div class="flex  justify-between items-center px-2 md:px-28 bg-white w-full">
+          <a href="./index.php"><img class="w-[180px]" src="../imgs/header_logo.png" alt=""></a>
+          <div class="text-right">
+          <button class="md:hidden md:text-right" onclick="handClick()">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          </button>
+            <ul id="main-menu" class=" gap-4 font-bold hidden md:flex items-end text-right     delay-150  ">
               <li class="hover:underline  hover:text-[#e76ea5]"><a href="../index.php">Trang chủ</a></li>
-              <li class="hover:underline  hover:text-[#e76ea5]"><a href="gioithieu.php">Giới thiệu</a></li>
+              <li class="hover:underline  hover:text-[#e76ea5]"><a href="./gioithieu.php">Giới thiệu</a></li>
 
               <li>
-                <a href="sanpham.php" class="hover:underline  hover:text-[#e76ea5]">Sản phẩm</a>
-                <ul class="list-sanPham px-4 w-[200px] border border-[#bbb] ">
+                <a href="./sanpham.php" class="hover:underline  hover:text-[#e76ea5]">Sản phẩm</a>
+                <ul class="list-sanPham  px-4 w-[200px]  border border-[#bbb] ">
                   <li class="text-[#777] hover:text-[#e76ea5] font-[400] border-b-2 py-2"><a href="">Hộp đơn</a></li>
                   <hr>
-                  <li class="text-[#777] hover:text-[#e76ea5] font-[400] border-b-2 py-2"><a href="">Combo 2 hộp</a>
-                  </li>
+                  <li class="text-[#777] hover:text-[#e76ea5] font-[400] border-b-2 py-2"><a href="">Combo 2 hộp</a></li>
                   <li class="text-[#777] hover:text-[#e76ea5] font-[400]  py-2"><a href="">Combo 3 hộp</a></li>
                 </ul>
                 <style>
@@ -93,8 +102,7 @@
               </li>
 
 
-              
-              <li class="hover:underline  hover:text-[#e76ea5]"><a href="lienHe.php">Liên hệ</a></li>
+              <li class="hover:underline  hover:text-[#e76ea5]"><a href="./lienHe.php">Liên hệ</a></li>
             </ul>
           </div>
         </div>
@@ -102,20 +110,21 @@
 
 
       </section>
-      <section class="">
-        <div>
-            <img class="absolute" src="../imgs/breadcrumb_img.png" alt="">
-            <div class=" py-12 pl-28 relative" >
-            <h2  class="text-white font-bold text-[38px] ">GIỚI THIỆU TAEHUI VIỆT NAM</h2>
-            <span class="text-white text-[17px] "><a href="index.php">Trang chủ</a>  <i class="fa fa-angle-right" aria-hidden="true"></i><a href=""> Giới thiệu TaeHui Việt Nam</a> </span>
+    </header>
+    <section class="">
+        <div
+        class=" mx-auto px-5 h-full w-full bg-no-repeat bg-cover"
+        class="w-full"  style="background-image: url('../imgs/breadcrumb_img.png') ;">
+            <div class=" py-3 md:py-10 md:pl-28 relative" >
+            <h2  class="text-white font-bold  text-[20px]  md:text-[38px] ">GIỚI THIỆU TAEHUI VIỆT NAM</h2>
+            <span class="text-white text-[17px] "><a href="index.php">Trang chủ</a>  <i class="fa fa-angle-right" aria-hidden="true"></i><a href=""> Liên hệ</a> </span>
         </div>
         </div>
     </section>
-    </header>
-    <main class="max-w-7xl m-auto text-[#545454]">
+    <main class="max-w-7xl m-auto text-[#545454] px-5">
       <article class="">
-        <h1 style="font-size:45px;" class="font-bold  text-[#545454]">Giới thiệu Taehui Việt Nam</h1>
-        <img class="imggg m-auto w-[600px] h-[161px] mt-10" src="../imgs/header_logo.png" alt="">
+        <h1 class="md:text-[45px] text-[25px] pt-5 font-bold  text-[#545454]">Giới thiệu Taehui Việt Nam</h1>
+        <img class="imggg m-auto md:w-[600px] md:h-[161px] mt-10" src="../imgs/header_logo.png" alt="">
         <article class="item-gt ">
           <h2 class="font-bold text-[26px] pb-4 pt-10 ">1. Ý nghĩa thương hiệu</h3>
             <p>Thị trường mỹ phẩm và làm đẹp tại Việt Nam đang có xu hướng tăng trưởng cao do nhu cầu ngày càng đa dạng
@@ -198,8 +207,8 @@
         </article>
       </article>
     </main>
-    <footer class="px-4 md:px-0 border-t-2 ">
-      <div class="max-w-7xl m-auto pt-8 grid grid-cols-4 gap-8">
+    <footer class="px-4 md:px-0 border-t-2  ">
+      <div class="max-w-7xl m-auto pt-8 grid md:grid-cols-4 gap-8">
         <div class="">
           <a href=""><img class="w-[180px] h-[48px] mb-6" src="../imgs/header_logo.png" alt=""></a>
           <span class=" font-[900] ">Công ty Cổ phần TaeHui Mang thanh xuân đến cho phụ nữ Việt</span>
@@ -242,6 +251,19 @@
 
     <body>
   </article>
+
+  <script>
+    function handClick() {
+    menu = document.getElementById('main-menu')
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none'
+    } else {
+        menu.style.display = 'block'
+    
+      }
+    }
+    
+  </script>
 </body>
 
 </html>
